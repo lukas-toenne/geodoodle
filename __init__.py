06f2f598993da526381230d825522689d156f6fb
Lukas Tönne <lukas.toenne@gmail.com>
2021-05-11 08:38:07 +0100

    Bump to Blender 2.93 and some more bl_info.

--- __init__.py
+++ __init__.py
@@ -24,11 +24,12 @@ bl_info = {
     "name": "GeoDoodle",
     "author": "Lukas Toenne",
     "version": (0, 1),
-    "blender": (2, 92, 0),
-    "location": "",
+    "blender": (2, 93, 0),
+    "location": "View3D > Object > Geodesic Distance",
     "description": "Compute geodesic distance using a heat method",
     "warning": "",
-    "doc_url": "",
+    "doc_url": "https://github.com/lukas-toenne/geodoodle",
+    "tracker_url": "https://github.com/lukas-toenne/geodoodle/issues",
     "category": "Mesh",
 }
 
